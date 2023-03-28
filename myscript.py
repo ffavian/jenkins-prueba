e2844f49faa4eb3a88d7a112b2db0ab1218de63e
Favio Ajhuacho <favio.favian@gmail.com>
2023-03-28 12:07:21 -0400

    script.py file created

---
+++ myscript.py
@@ -0,0 +1,16 @@
+import subprocess
+import time
+
+# Configurar la ruta al directorio del repositorio
+repo_dir = './jenkins-prueba'
+
+while True:
+    # Ejecutar el comando git pull en el directorio del repositorio
+    result = subprocess.run(['git', 'pull'], cwd=repo_dir)
+
+    # Comprobar si hubo algún error en la ejecución del comando git pull
+    if result.returncode != 0:
+        print('Error al ejecutar el comando git pull')
+
+    # Esperar 12 horas antes de ejecutar el siguiente git pull
+    time.sleep(3 * 60)
